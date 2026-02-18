23674452a9a5e1f8b57cf5dd02e78afea5a22f34
anurag <yourboyanurag777@gmail.com>
2026-02-18 15:25:14 +0530

    database connection

---
+++ backend/db/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose"
+
+const connectDb = async ()=>{
+    try {
+        mongoose.connect(process.env.MONGO_URI || 3000);
+        console.log("mongodb connected")
+    } catch (error) {
+        console.log(error)
+        
+    }
+}
