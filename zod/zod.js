0ed01c895391a6884e771e3e4f5dbde4db3f1ce4
anurag <yourboyanurag777@gmail.com>
2026-02-18 15:50:29 +0530

    userschema

--- zod/zod.js
+++ zod/zod.js
@@ -1,1 +1,7 @@
-import {z} from "zod"
+import { z} from "zod"
+export const userSchema = z.object({
+    username:z.string().min(3).max(20),
+    email:z.string().email(),
+    password:z.string().min(8).max(20),
+
+})
