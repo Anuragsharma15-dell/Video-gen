4f71b27cfcf70936502809d783ee4bb6d659e220
anurag <yourboyanurag777@gmail.com>
2026-02-19 11:40:10 +0530

    main-server-file

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,11 @@
+
+import dotenv from "dotenv"
+dotenv.config();
 import express from "express"
 const app = express();
-import connectDb from "./db/db";
+import connectDb from "./db/db.js";
+import cors from "cors"
+import router from "../routes/route.js"
 
 
 app.use(express.json());
@@ -8,6 +13,11 @@ app.use(cors());
 
 connectDb();
 
+
+app.use("/api", router);
+app.get("/", (req, res) => {
+    res.send("Hello World")
+})
 
 
 
@@ -18,5 +28,5 @@ connectDb();
 
 
 app.listen(process.env.PORT || 5000, () => {
-    console.log("Server is running on port 5000")
+    console.log(`Server is running on port ${process.env.PORT}`)
 })
